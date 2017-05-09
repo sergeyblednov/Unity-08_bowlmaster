29ee081f8bc2349014765674e2cf94271ee3124e
Sergey Blednov <sergey.blednov@pixacore.com>
2017-05-09 11:36:13 -0400

    Score master tests

--- Assets/Editor/ScoreDisplayTest.cs
+++ Assets/Editor/ScoreDisplayTest.cs
@@ -14,7 +14,98 @@ public class ScoreDisplayTest {
 
 	[Test]
 	public void T01Bowl1 () {
-		ScoreDisplay sd = new ScoreDisplay ();
-		sd.FillRolls(new List<int>());
+		int[] rolls = { 1 };
+		string rollsString = "1";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
 	}
+
+	[Test]
+	public void T02Bowl3 () {
+		int[] rolls = { 1, 8, 3 };
+		string rollsString = "183";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
+	}
+
+	[Test]
+	public void T03BowlWithSpire01 () {
+		int[] rolls = { 1, 8, 2, 8, 5 };
+		string rollsString = "182/5";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
+	}
+
+	[Test]
+	public void T04BowlWithSpire02 () {
+		int[] rolls = { 1, 9, 3, 5, 5 };
+		string rollsString = "1/355";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
+	}
+
+	[Test]
+	public void T05BowlWithSpire03 () {
+		int[] rolls = { 1, 9 };
+		string rollsString = "1/";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
+	}
+
+	[Test]
+	public void T06BowlWithStrike () {
+		int[] rolls = { 10, 3, 3, 10, 5 };
+		string rollsString = "X 33X 5";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
+	}
+
+	// http://slocums.homestead.com/gamescore.html
+	[Test]
+	[Category ("Verification")]
+	public void TG02GoldenCopyA () {
+		int[] rolls = { 10, 7,3, 9,0, 10, 0,8, 8,2, 0,6, 10, 10, 10,8,1};
+		string rollsString = "X 7/9-X -88/-6X X X81";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
+	}
+
+	//http://guttertoglory.com/wp-content/uploads/2011/11/score-2011_11_28.png
+	[Category ("Verification")]
+	[Test]
+	public void TG03GoldenCopyB1of3 () {
+		int[] rolls = { 10, 9,1, 9,1, 9,1, 9,1, 7,0, 9,0, 10, 8,2, 8,2,10};
+		string rollsString = "X 9/9/9/9/7-9-X 8/8/X";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
+	}
+
+	//http://guttertoglory.com/wp-content/uploads/2011/11/score-2011_11_28.png
+	[Category ("Verification")]
+	[Test]
+	public void TG03GoldenCopyB2of3 () {
+		int[] rolls = { 8,2, 8,1, 9,1, 7,1, 8,2, 9,1, 9,1, 10, 10, 7,1};
+		string rollsString = "8/819/718/9/9/X X 71";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
+	}
+
+	//http://guttertoglory.com/wp-content/uploads/2011/11/score-2011_11_28.png
+	[Category ("Verification")]
+	[Test]
+	public void TG03GoldenCopyB3of3 () {
+		int[] rolls = { 10, 10, 9,0, 10, 7,3, 10, 8,1, 6,3, 6,2, 9,1,10};
+		string rollsString = "X X 9-X 7/X 8163629/X";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
+	}
+
+	// http://brownswick.com/wp-content/uploads/2012/06/OpenBowlingScores-6-12-12.jpg
+	[Category ("Verification")]
+	[Test]
+	public void TG03GoldenCopyC1of3 () {
+		int[] rolls = { 7,2, 10, 10, 10, 10, 7,3, 10, 10, 9,1, 10,10,9};
+		string rollsString = "72X X X X 7/X X 9/XX9";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
+	}
+
+	// http://brownswick.com/wp-content/uploads/2012/06/OpenBowlingScores-6-12-12.jpg
+	[Category ("Verification")]
+	[Test]
+	public void TG03GoldenCopyC2of3 () {
+		int[] rolls = { 10, 10, 10, 10, 9,0, 10, 10, 10, 10, 10,9,1};
+		string rollsString = "X X X X 9-X X X X X91";
+		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls (rolls.ToList ()));
+	}
+
 }
--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -21,8 +21,42 @@ public class ScoreDisplay : MonoBehaviour {
 		}
 	}
 
-	static string FormatRolls (List<int> rolls) {
+	public static string FormatRolls (List<int> rolls) {
 		string output = "";
+
+		// My version
+
+//		for (int i = 0; i < rolls.Count; i++) {
+//			if (rolls[i] == 10) {
+//				output += "X";
+//				if (output.Length < 19) {
+//					output += " ";
+//				}
+//			} else if (i >= 1 && (rolls[i - 1] + rolls [i] == 10) && output.Length % 2 == 0) {
+//				output += "/";
+//			} else {
+//				output += rolls [i].ToString ();	
+//			}
+//		}
+
+		// Ben version
+
+		for (int i = 0; i < rolls.Count; i++) {
+			int box = output.Length + 1;
+
+			if (rolls [i] == 0) {
+				output += "-";
+			} else if (box % 2 == 0 && (rolls [i - 1] + rolls [i] == 10)) {
+				output += "/";
+			} else if (rolls [i] == 10 && box >= 19) {
+				output += "X";
+			} else if (rolls [i] == 10) {
+				output += "X ";
+			} else {
+				output += rolls [i].ToString ();	
+			}
+		}
+
 
 		return output;
 	}
